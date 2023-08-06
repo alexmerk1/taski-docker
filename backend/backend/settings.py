1efa41030c1e45a286fa685e3c7a086b65f27691
Александр Меркулов <Alexmerk111@yandex.ru>
2023-08-06 11:57:59 +0300

    edit static 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -108,9 +108,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static_backend/'
-
-STATIC_ROOT = BASE_DIR / 'static_backend'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
